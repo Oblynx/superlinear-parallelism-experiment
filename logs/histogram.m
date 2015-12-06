3aa3e6cb3951a2356ae443306b0b8304508cd65a
Konstantinos Samaras-Tsakiris <kisamara@auth.gr>
2015-12-06 19:17:42 +0200

    Debugging

---
+++ logs/histogram.m
@@ -0,0 +1,7 @@
+function histogram(bins)
+load('search');
+figure(1);
+hist(search,str2double(bins));
+dMEAN= mean(search)
+dSTD= std(search)
+end
--- src/searchSim.cpp
+++ src/searchSim.cpp
@@ -1,11 +1,10 @@
-//! Random walk simulator -- any number of dims|particles
-//! Particles move independently
+//! Search simulator -- any number of dims|particles
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
-#define WORLD_BOUNDS 21
 #define WORLD_SHIFT ((WORLD_BOUNDS-1)/2)
 template<int N, int P> struct Power {
   enum { val = N * Power<N, P-1>::val };
@@ -20,12 +19,12 @@ template<> struct WorldCenter<-1>{
   enum{ val= 0 };
 };
 template<int D, int dimCache= D> struct Address{
-  static int val(int *position){
+  static int val(const int *position){
     return position[D]*Power<WORLD_BOUNDS,D>::val + Address<D-1, dimCache>::val(position);
   }
 };
 template<int dimCache> struct Address<-1,dimCache>{
-  static int val(int*) { return WorldCenter<dimCache>::val; }
+  static int val(const int*) { return WorldCenter<dimCache>::val; }
 };
 
 //bit-hacks
@@ -37,19 +36,16 @@ template<int dimCache> struct Address<-1,dimCache>{
   return r;
 }*/
 __attribute__((const)) inline unsigned log2_32(unsigned v) {
-  static const int MultiplyDeBruijnBitPosition[32] = 
-  {
-      0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+  static const int MultiplyDeBruijnBitPosition[32] = {
+        0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
         8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
   };
-
   v |= v >> 1; // first round down to one less than a power of 2 
   v |= v >> 2;
   v |= v >> 4;
   v |= v >> 8;
   v |= v >> 16;
-
-  return MultiplyDeBruijnBitPosition[(uint32_t)(v * 0x07C4ACDDU) >> 27];
+  return MultiplyDeBruijnBitPosition[(unsigned)(v * 0x07C4ACDDU) >> 27];
 }
 
 template<unsigned dimensions>
@@ -61,6 +57,7 @@ public:
   }
   inline void setWorld(unsigned* world) { world_= world; }
   inline void move(){ search(); }
+  //inline void move(){ randomWalk(); }
 
   int position_[dimensions];
 private:
@@ -69,7 +66,7 @@ private:
     //Read nearby position values
     //Exclude out-of-bounds -> if out-of-bounds, address:= -1
     //Pick rand
-    //Select biasing on vals
+    //Select biasing on world vals 
     
     // Copy position locally
     int localPos[dimensions];
@@ -78,6 +75,11 @@ private:
     // Increase current world position
     address_= Address<dimensions-1>::val(localPos);
     if (!(world_[address_] & 0x40000000)) world_[address_]<<= 1;  //Select bit#30
+    
+    /*DEBUG*/cout << "[world]: @"<<address_<<"="<<world_[address_]<<'\t';
+    cout << "[pos]: ";
+    for(int i=0; i<dimensions; i++) cout<<position_[i]<<',';
+    cout <<'\n';
 
     // Calc addresses of nearby positions
     for(unsigned i=0; i<dimensions; i++){
@@ -99,18 +101,49 @@ private:
         localPos[i]++;
       }
     }
-
+    
     // Pick random (weighted) position
-    short localWeights[2*dimensions], maxWeight=0;
-    for(unsigned i=0; i<2*dimensions; i++){
+    short randS= log2_32((unsigned)rand());   // 30..0 exponentially distributed
+    short localWeights[2*dimensions], maxWeight=0, minWeight=30; 
+    short maxLessRand=0, maxLessRandI[2*dimensions], j=0;
+    for(short i=0; i<2*dimensions; i++){
       localWeights[i]= 30-log2_32(world_[nearbyAddress_[i]]);
       maxWeight= (localWeights[i] > maxWeight)? localWeights[i]: maxWeight;
+      minWeight= (localWeights[i] < minWeight)? localWeights[i]: minWeight;
     }
     //normalize weights so that maximum becomes 30
-    if (maxWeight < 30) for(unsigned i=0; i<2*dimensions; i++) localWeights+= 30-maxWeight;
-    short randS= log2_32((unsigned)rand());   // 30..0 exponentially distributed
+    if (maxWeight < 30){
+      short normalizer= 30-maxWeight;
+      for(unsigned i=0; i<2*dimensions; i++) localWeights[i]+= normalizer;
+      minWeight+= normalizer;
+    }
+    j=0;
+    if (minWeight <= randS) {
+      for(short i=0; i<2*dimensions; i++) {
+        if (localWeights[i] == maxLessRand) maxLessRandI[j]= i, j++;
+        else if ((localWeights[i] > maxLessRand) && (localWeights[i] <= randS))
+               maxLessRand= localWeights[i], j=0, maxLessRandI[j]= i, j++;
+        //cout<<"\t[s::for]: Checked move: "<<i<<", "<<localWeights[i]<<", "<<randS <<"\n";
+      }
+    } else {    // In that case, simply pick everything equal to minWeight
+      for(short i=0; i<2*dimensions; i++){
+        if (localWeights[i] == minWeight) maxLessRandI[j]= i, j++;
+      }
+    }
+    cout << "[search]: After picking: "<<randS<<", "<<j<<"\n";
+    j= randS%j;
+    
+    //Picked nearbyAddress_[maxLessRandI[j]]
+    j= maxLessRandI[j];
+    address_= nearbyAddress_[j];
+    if (j < dimensions) position_[j]++;
+    else position_[j-dimensions]--;
 
+    cout << "[new pos]: ";
+    for(int i=0; i<dimensions; i++) cout<<position_[i]<<',';
+    cout << "\tj="<<j<<'\n';
 
+    cout<<"[search]: exiT\n";
   }
   inline void randomWalk(){
     const unsigned i= rand()%(2*dimensions);
@@ -125,6 +158,10 @@ template<unsigned num_particles= 1, unsigned dimensions= 2, unsigned target_dist
 class Simulation {
   public:
     Simulation(ostream& s): outs_(s){
+      static_assert(dimensions<=15, "ERROR: Too many dimensions");
+      static_assert(dimensions>0, "Dimensions <= 0");
+      static_assert(target_distance<=WORLD_SHIFT, "target out-of-bounds");
+      static_assert(num_particles > 0, "nmu_particles <= 0");
       for(unsigned i=0; i<num_particles; i++) particles_[i].setWorld(world_);
       for(unsigned i=0; i<Power<WORLD_BOUNDS,dimensions>::val; i++) world_[i]=0;
       for(unsigned i=0; i<dimensions; i++) targetPos_[i]= 0;
@@ -132,7 +169,7 @@ class Simulation {
     }
     void run_for(int experimentRuns){
       unsigned time;
-      outs_<<"Experiments number: "<<experimentRuns<<'\n';
+      //outs_<<"Experiments number: "<<experimentRuns<<'\n';
       for(int i=0; i<experimentRuns; i++){
         time= simulate();
         outs_<<time<<'\n'; 
@@ -169,7 +206,10 @@ unsigned Simulation<num_particles,dimensions,target_distance>::simulate(){
 }
 
 int main(int argc, char **argv){
-  Simulation<1,1,3> sim(cout);
+  static_assert(sizeof(unsigned) == 4, "Bithacks can't work on this system");
+  //srand(time(0));
+  srand(1);
+  Simulation<NUM_PARTICLES,DIMENSIONS,TARGET_DISTANCE> sim(cout);
   unsigned times=1000;
   if(argc == 2) times= strtol(argv[1], NULL, 10);
   sim.run_for(times);
